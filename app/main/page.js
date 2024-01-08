c6011d5002a2ff4609c0fb04a470629964c2382a
Vipin Kumar <vipinkumar@Salonis-MacBook-Air.local>
2024-01-08 14:59:25 +0530

    first commit

---
+++ app/main/page.js
@@ -0,0 +1,19 @@
+import React,{ useEffect, useState } from "react";
+
+const Main = () => {
+    const [apiData, setApiData] = useState([]);
+    useEffect(() => {
+      fetch("https://fakestoreapi.com/products/")
+        .then((res) => res.json())
+        .then((json) => setApiData(json));
+    }, []);
+    return (
+      <>
+        {apiData.map((item) => {
+          return <div>{item.title}</div>;
+        })}
+      </>
+    );
+}
+ 
+export default Main;
---
+++ app/profile/page.js
@@ -0,0 +1,9 @@
+import About from "../about/page";
+
+const Profile = () => {
+    return ( <div>
+        <About/>
+    </div> );
+}
+ 
+export default Profile;
